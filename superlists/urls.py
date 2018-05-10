92c38da56e4fff02dfb6d58fb93bc2f974d71c58
yuebei <yuebei58@gmail.com>
2018-05-10 16:50:14 +0800

    set STATIC_ROOT in settings and disable admin

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+# from django.contrib import admin
 
 from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
 ]
