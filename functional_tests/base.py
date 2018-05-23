8ffaf3a5ba10d26112a5b6d76d46d23bb315f78f
yuebei <yuebei58@gmail.com>
2018-05-23 17:13:56 +0800

    fixed one bug

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -9,6 +9,7 @@ MAX_WAIT = 10
 
 def wait(fn):
     def modified_fn(*args,**kwargs):
+        start_time = time.time()
         while True:
             try:
                 return fn(*args, **kwargs)
