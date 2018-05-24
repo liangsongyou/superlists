a9ed4457ee6f37cddcd12485110c673e0b3fc1e5
yuebei <yuebei58@gmail.com>
2018-05-24 17:24:13 +0800

    add command_tolls

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,8 +1,10 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from .server_tools import reset_database
 import time
 import os
+
 
 
 MAX_WAIT = 10
@@ -27,6 +29,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
