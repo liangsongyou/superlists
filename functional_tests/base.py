bdac74349dec8484460eb18cb65de86045432f03
yuebei <yuebei58@gmail.com>
2018-05-29 18:04:44 +0800

    fixed template bugs

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -45,7 +45,7 @@ class FunctionalTest(StaticLiveServerTestCase):
                 os.makedirs(SCREEN_DUMP_LOCATION)
             for ix, handle in enumerate(self.browser.window_handles):
                 self._windowid = ix
-                self.browser.swithch_to_window(handle)
+                self.browser.switch_to_window(handle)
                 self.take_screenshot()
                 self.dump_html()
         self.browser.quit()
